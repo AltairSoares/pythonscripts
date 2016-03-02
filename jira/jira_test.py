393823e4bdc62d75e3de35cb4adda96138403e58
Altair Soares <altairum@gmail.com>
2016-03-01 23:31:11 -0400

    Adição de chamado JIra

--- jira/jira_test.py
+++ jira/jira_test.py
@@ -9,9 +9,9 @@ with open("config.yml", 'r') as ymlfile:
 
 #Prints parsed variables
 
-print(cfg['servercfg']['server'])
-print(cfg['servercfg']['user'])
-print(cfg['servercfg']['pwd'])
+#print(cfg['servercfg']['server'])
+#print(cfg['servercfg']['user'])
+#print(cfg['servercfg']['pwd'])
 
 
 
@@ -23,13 +23,24 @@ options = {
 jira = JIRA(options, basic_auth=(cfg['servercfg']['user'],cfg['servercfg']['pwd']))
 
 print("connected")
-print(projects = jira.projects())
 
-'''issue_dict = {
-    'project': {'id': 123},
+issue_dict = {
+    'project': 'SD',
     'summary': 'New issue from jira-python',
     'description': 'Look into this one',
-    'issuetype': {'name': 'Bug'},
-}
-new_issue = jira.create_issue(fields=issue_dict)'''
+    'issuetype': {'name': 'Incidente'},
+    'customfield_12100': 'Zabbix',
+    'customfield_12106': '4',
+    'customfield_12107': '32',
+    'customfield_12108': '224',
+    'customfield_11402': '9999',
 
+}
+new_issue = jira.create_issue(fields=issue_dict)
+
+print(new_issue)
+
+#Elemento 12108 = 224
+#Descrição do Serviço  12107= 32
+#Categoria 12106 = 4
+#Solicitante 12100
